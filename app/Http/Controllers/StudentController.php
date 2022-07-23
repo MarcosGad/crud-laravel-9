1f1cb410ade16d51933ddf3411dc250c4e6a5a2b
MarcosGad <marcosgad@yahoo.com>
2022-07-23 14:13:27 +0200

    edit

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -56,18 +56,47 @@ class StudentController extends Controller
    
     public function edit(Student $student)
     {
-        //
+        return view('edit', compact('student'));
     }
 
     
     public function update(Request $request, Student $student)
     {
-        //
+        $request->validate([
+            'student_name'      =>  'required',
+            'student_email'     =>  'required|email',
+            'student_image'     =>  'image|mimes:jpg,png,jpeg,gif,svg|max:2048|dimensions:min_width=100,min_height=100,max_width=1000,max_height=1000'
+        ]);
+
+        $student_image = $request->hidden_student_image;
+
+        if($request->student_image != '')
+        {
+            $student_image = time() . '.' . request()->student_image->getClientOriginalExtension();
+
+            request()->student_image->move(public_path('images'), $student_image);
+        }
+
+        $student = Student::find($request->hidden_id);
+
+        $student->student_name = $request->student_name;
+
+        $student->student_email = $request->student_email;
+
+        $student->student_gender = $request->student_gender;
+
+        $student->student_image = $student_image;
+
+        $student->save();
+
+        return redirect()->route('students.index')->with('success', 'Student Data has been updated successfully');
     }
 
     
     public function destroy(Student $student)
     {
-        //
+        $student->delete();
+
+        return redirect()->route('students.index')->with('success', 'Student Data deleted successfully');
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,7 +38,13 @@
 						<td>{{ $row->student_email }}</td>
 						<td>{{ $row->student_gender }}</td>
 						<td>
-							<a href="{{ route('students.show', $row->id) }}" class="btn btn-primary btn-sm">View</a>
+							<form method="post" action="{{ route('students.destroy', $row->id) }}">
+								@csrf
+								@method('DELETE')
+								<a href="{{ route('students.show', $row->id) }}" class="btn btn-primary btn-sm">View</a>
+								<a href="{{ route('students.edit', $row->id) }}" class="btn btn-warning btn-sm">Edit</a>
+								<input type="submit" class="btn btn-danger btn-sm" value="Delete" />
+							</form>
 						</td>
 					</tr>
 
